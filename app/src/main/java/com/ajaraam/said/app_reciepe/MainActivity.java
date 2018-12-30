8eb2e9b5a91090d29e1f922fb3b1cd1a8541b4ad
said ajaraam <saidajaraam7@gmail.com>
2018-12-30 22:48:46 +0100

    Reciepe app

---
+++ app/src/main/java/com/ajaraam/said/app_reciepe/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ajaraam.said.app_reciepe;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+private static int SPLACH_TIME_OUT=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boolean b = new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+        },SPLACH_TIME_OUT);
+    }
+}
